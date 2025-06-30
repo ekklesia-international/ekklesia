9d4560916c05064acf5d97ec5495ef8e7cefb732
Sanu G Stanly <sstanly@corcentric.com>
2025-06-30 11:49:10 +0530

    init

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "check very thing is fine",
+  description: 'sasasassasa',
+  ogImage: "http://ekklesiainternational.org/assets/_number40.jpg",
+  ogTitle: "check very thing is fine",
+  ogDescription: 'sasasassasa',
+})
+const version = 2 + 1
+</script>
+
+<template>
+  <div class="hello">
+    Hello Nuxt {{ version }}!
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
