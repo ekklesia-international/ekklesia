a16d289797137270041a17256d1d9fd162c72b90
Sanu G Stanly <sstanly@corcentric.com>
2025-07-08 17:11:56 +0530

    added sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nuxt-svgo'],
+  modules: ['@nuxt/ui', 'nuxt-svgo', '@nuxtjs/sitemap'],
   css: ['~/assets/css/main.css'],
+  site: {
+    url: 'https://ekklesiainternational.org',
+    name: 'EKKLESIA WORSHIP CENTER'
+  },
   vite: {
     plugins: [
       tailwindcss(),
